94478aaf828a941988f61e1154ada206ad673976
pandaman <kointosudesuyo@infoseek.jp>
2017-09-27 16:42:33 +0900

    Implemented skew heaps

--- src/main.rs
+++ src/main.rs
@@ -3,199 +3,56 @@
 extern crate test;
 extern crate rand;
 
-trait Heap<T> {
-    fn add(&mut self, v: T);
-    fn empty(&self) -> bool;
-    fn get_min(&self) -> Option<T>;
-    fn remove_min(&mut self) -> Option<T>;
-}
-
-struct BinaryHeap<T> {
-    buffer: Vec<T>
-}
-
-impl<T: Clone + Ord + Default> BinaryHeap<T> {
-    fn new() -> Self {
-        BinaryHeap{ buffer: vec![ Default::default() ] }
-    }
-
-    fn size(&self) -> usize {
-        self.buffer.len() - 1
-    }
-
-    fn upheap(&mut self, idx: usize) {
-        if idx <= 1 {
-            return;
-        }
-        if self.buffer[idx / 2] > self.buffer[idx] {
-            self.buffer.swap(idx / 2, idx);
-            self.upheap(idx / 2);
-        }
-    }
-
-    fn downheap(&mut self, idx: usize) {
-        if idx * 2 > self.size() {
-            return;
-        }
-        if idx * 2 + 1 <= self.size() && self.buffer[idx * 2 + 1] < self.buffer[idx * 2] && self.buffer[idx * 2 + 1] < self.buffer[idx] {
-            self.buffer.swap(idx * 2 + 1, idx);
-            self.downheap(idx * 2 + 1);
-        } else if self.buffer[idx * 2] < self.buffer[idx] {
-            self.buffer.swap(idx * 2, idx);
-            self.downheap(idx * 2);
-        }
-    }
-}
-
-impl<T: Clone + Ord + Default> Heap<T> for BinaryHeap<T> {
-    fn add(&mut self, v: T) {
-        self.buffer.push(v);
-        let idx = self.size();
-        self.upheap(idx);
-    }
-
-    fn empty(&self) -> bool {
-        self.size() == 0
-    }
-
-    fn get_min(&self) -> Option<T> {
-        if self.empty() {
-            None
-        } else {
-            let idx = self.size();
-            Some(self.buffer[idx].clone())
-        }
-    }
-
-    fn remove_min(&mut self) -> Option<T> {
-        if !self.empty() {
-            let top = self.buffer.swap_remove(1);
-            self.downheap(1);
-            Some(top)
-        } else {
-            None
-        }
-    }
-}
-
-struct PairingTree<T> {
-    top: T,
-    subheaps: Vec<PairingTree<T>>
-}
-
-impl<T: Ord> PairingTree<T> {
-    fn merge_in_place(&mut self, mut other: Self) {
-        if self.top > other.top {
-            std::mem::swap(self, &mut other);
-        }
-        self.subheaps.push(other);
-    }
-}
-
-enum PairingHeap<T> {
-    Empty,
-    Tree(PairingTree<T>)
-}
-
-impl<T: Clone + Ord> PairingHeap<T> {
-    fn new() -> Self {
-        PairingHeap::Empty
-    }
-
-    fn single(v: T) -> Self {
-        PairingHeap::Tree(PairingTree {
-            top: v,
-            subheaps: vec![]
-        })
-    }
-
-    fn merge(mut self, other: Self) -> Self {
-        self.merge_in_place(other);
-        self
-    }
-
-    fn merge_in_place(&mut self, mut other: Self) {
-        use PairingHeap::*;
-        use std::mem::swap;
-        match *self {
-            Empty => swap(self, &mut other),
-            Tree(ref mut tree) => 
-                match other {
-                    Empty => return,
-                    Tree(other) => tree.merge_in_place(other)
-                }
-        }
-    }
-
-    fn merge_pairs<I: Iterator<Item=PairingTree<T>>>(mut iter: I) -> PairingHeap<T> {
-        use PairingHeap::*;
-        match iter.next() {
-            None => Empty,
-            Some(mut first) =>
-                match iter.next() {
-                    None => Tree(first),
-                    Some(second) => {
-                        first.merge_in_place(second);
-                        Tree(first).merge(Self::merge_pairs(iter))
-                    }
-                }
-        }
-    }
-}
-
-impl<T: Ord + Clone> Heap<T> for PairingHeap<T> {
-    fn add(&mut self, v: T) {
-       self.merge_in_place(PairingHeap::single(v));
-    }
-
-    fn empty(&self) -> bool {
-        use PairingHeap::*;
-        match *self {
-            Empty => true,
-            _ => false
-        }
-    }
-
-    fn get_min(&self) -> Option<T> {
-        use PairingHeap::*;
-        match *self {
-            Empty => None,
-            Tree(ref tree) => Some(tree.top.clone())
-        }
-    }
-
-    fn remove_min(&mut self) -> Option<T> {
-        use PairingHeap::*;
-        use std::mem::replace;
-        match replace(self, Empty) {
-            Empty => None,
-            Tree(tree) => {
-                *self = Self::merge_pairs(tree.subheaps.into_iter());
-                Some(tree.top)
-            }
-        }
-    }
-}
-
-fn test(heap: &mut Heap<i32>) {
-    heap.add(1);
-    heap.add(0);
-    heap.add(6);
-    heap.add(4);
-    heap.add(-6);
-    heap.add(5);
-    heap.add(3);
-
-    while !heap.empty() {
-        println!("{:?}", heap.remove_min());
-    }
-}
+mod heap;
+mod binary;
+mod pairing;
+mod skew;
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use test::Bencher;
     use rand::Rng;
+    use heap::Heap;
+    use binary::BinaryHeap;
+    use pairing::PairingHeap;
+    use skew::SkewHeap;
+    
+    fn test(heap: &mut Heap<i32>) {
+        heap.add(1);
+        heap.add(0);
+        heap.add(6);
+        heap.add(4);
+        heap.add(-6);
+        heap.add(5);
+        heap.add(3);
+
+        assert_eq!(heap.remove_min(), Some(-6));
+        assert_eq!(heap.remove_min(), Some(0));
+        assert_eq!(heap.remove_min(), Some(1));
+        assert_eq!(heap.remove_min(), Some(3));
+        assert_eq!(heap.remove_min(), Some(4));
+        assert_eq!(heap.remove_min(), Some(5));
+        assert_eq!(heap.remove_min(), Some(6));
+    }
+
+    #[test]
+    fn test_binary() {
+        let mut heap = BinaryHeap::<i32>::new();
+        test(&mut heap);
+    }
+
+    #[test]
+    fn test_pairing() {
+        let mut heap = PairingHeap::<i32>::new();
+        test(&mut heap);
+    }
+
+    #[test]
+    fn test_skew() {
+        let mut heap = SkewHeap::<i32>::new();
+        test(&mut heap);
+    }
 
     #[inline]
     fn bench<H: Heap<i32>, R: Rng>(heap: &mut H, rng: &mut R) {
@@ -223,9 +80,15 @@ mod tests {
 
         b.iter(|| bench(&mut heap, &mut rng));
     }
+
+    #[bench]
+    fn bench_skew(b: &mut Bencher) {
+        let mut rng = rand::IsaacRng::new_unseeded();
+        let mut heap = SkewHeap::<i32>::new();
+
+        b.iter(|| bench(&mut heap, &mut rng));
+    }
 }
 
 fn main() {
-    let mut heap = PairingHeap::new();
-    test(&mut heap);    
 }
---
+++ src/skew.rs
@@ -0,0 +1,87 @@
+use heap::Heap;
+
+pub struct SkewTree<T> {
+    value: T,
+    left: Box<SkewHeap<T>>,
+    right: Box<SkewHeap<T>>,
+}
+
+pub enum SkewHeap<T> {
+    Empty,
+    Tree(SkewTree<T>)
+}
+
+impl<T: Ord> SkewHeap<T> {
+    pub fn new() -> Self {
+        self::SkewHeap::Empty
+    }
+
+    fn single(value: T) -> Self {
+        use self::SkewHeap::*;
+        Tree(SkewTree {
+            value: value,
+            left: Box::new(Empty),
+            right: Box::new(Empty)
+        })
+    }
+
+    fn merge_in_place(&mut self, mut other: Self) {
+        use std::mem::swap;
+        use self::SkewHeap::*;
+        match *self {
+            Empty => swap(self, &mut other),
+            Tree(ref mut this) => 
+                match other {
+                    Empty => return,
+                    Tree(mut other) => {
+                        if this.value > other.value {
+                            swap(this, &mut other)
+                        }
+                        this.right.merge_in_place(Tree(other));
+                        swap(&mut this.left, &mut this.right);
+                    }
+                }
+        }
+    }
+}
+
+impl<T: Ord + Clone> Heap<T> for SkewHeap<T> {
+    fn add(&mut self, value: T) {
+        self.merge_in_place(SkewHeap::single(value))
+    }
+
+    fn empty(&self) -> bool {
+        use self::SkewHeap::*;
+        match *self {
+            Empty => true,
+            _ => false
+        }
+    }
+
+    fn get_min(&self) -> Option<T> {
+        use self::SkewHeap::*;
+        match *self {
+            Empty => None,
+            Tree(SkewTree { ref value, .. }) => Some(value.clone())
+        }
+    }
+
+    fn remove_min(&mut self) -> Option<T> {
+        use std::mem::replace;
+        use self::SkewHeap::*;
+
+        match replace(self, Empty) {
+            Empty => None,
+            Tree(SkewTree {
+                value,
+                mut left,
+                right,
+            }) => {
+                left.merge_in_place(*right);
+                *self = *left;
+                Some(value)
+            }
+        }
+    }
+}
+
